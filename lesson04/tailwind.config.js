053cd7305ecd84401ae0644df313da9fccdbd087
magdielian-code <oziomaagaecheta81@gmail.com>
2024-06-12 22:59:56 +0100

    v4.0 initialization

--- lesson04/tailwind.config.js
+++ lesson04/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       screens: {
         'widescreen': {'raw': '(min-aspect-ratio: 3/2)'},
-        'tallscreen': {'raw': '(min-aspect-ratio: 1/2)'},
+        'tallscreen': {'raw': '(min-aspect-ratio: 13/20)'},
       }
     },
   },
