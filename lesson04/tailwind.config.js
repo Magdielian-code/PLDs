efcc5305f8590cdc7183907af5a80cea933b56b4
magdielian-code <oziomaagaecheta81@gmail.com>
2024-06-12 22:43:42 +0100

    started v4.0 Acme webiste production

---
+++ lesson04/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/index.html"],
+  theme: {
+    extend: {
+      screens: {
+        'widescreen': {'raw': '(min-aspect-ratio: 3/2)'},
+        'tallscreen': {'raw': '(min-aspect-ratio: 1/2)'},
+      }
+    },
+  },
+  plugins: [],
+}
+
