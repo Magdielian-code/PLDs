913724addbee3e400c4ca2f717d4071afe7451f5
magdielian-code <oziomaagaecheta81@gmail.com>
2024-06-13 01:41:37 +0100

    feat: made nav manu for mobile view functional.

--- lesson04/tailwind.config.js
+++ lesson04/tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
         'widescreen': {'raw': '(min-aspect-ratio: 3/2)'},
         'tallscreen': {'raw': '(min-aspect-ratio: 13/20)'},
       }
+      keyframes: {
+        'open-menu': {
+          '0%': {transform: 'scaleY(0)'},
+          '80%': {transform: 'scaleY(1.2)'},
+          '100%': {transform: 'scaleY(1)'},
+        }
+      },
+      asnimation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      }
     },
   },
   plugins: [],
