eab797ee34ef477c0e0eaf539f9716b6f6ef7642
magdielian-code <oziomaagaecheta81@gmail.com>
2024-06-13 16:36:27 +0100

    fixed tailwind.config.js file

--- v05/tailwind.config.js
+++ v05/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html", "./build/js/*.js"],
+  content: ["./build/**/*.{html,js}"],
   theme: {
     extend: {
       screens: {
