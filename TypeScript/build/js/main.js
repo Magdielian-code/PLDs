ebe53d4d57eacc50782d5380c858ff42055af25d
magdielian-code <oziomaagaecheta81@gmail.com>
2024-06-15 02:13:22 +0100

    ts data types

--- TypeScript/build/js/main.js
+++ TypeScript/build/js/main.js
@@ -1,3 +1,17 @@
 "use strict";
-let username = 'Ozioma Agaecheta';
-console.log(username);
+let myName;
+let meaningOfLife;
+let isLoading;
+let album;
+let dave;
+album = 54;
+dave = 65;
+dave = 'come here';
+dave = true;
+myName = 'Ozioma';
+meaningOfLife = 3;
+isLoading = true;
+console.log('Hi, my name is ' + { myName } + ' and the ' + { meaningOfLife } + 'is' + { isLoading });
+const sum = (a, b) => {
+    return a + b;
+};
--- TypeScript/src/main.ts
+++ TypeScript/src/main.ts
@@ -1,5 +1,20 @@
-let username = 'Ozioma Agaecheta'
-console.log(username)
+let myName: string;
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+let dave: string | number | boolean;
 
-let a = 12
-let b = '6'
+album = 54
+dave = 65
+dave = 'come here'
+dave = true
+
+myName = 'Ozioma'
+meaningOfLife = 3
+isLoading = true
+
+console.log('Hi, my name is ' + {myName} + ' and the ' + {meaningOfLife} + 'is' + {isLoading})
+
+const sum = (a: number , b: number) => {
+    return a + b
+}  
